7b143fea25ccd5ed3d6d4e4e708b1f4782ac3355
wooniwinnie <wooniwinnie@gmail.com>
2023-06-01 22:44:38 +0900

    react-tsx 공부 시작

---
+++ src/components/Hello.tsx
@@ -0,0 +1,15 @@
+// Hello는 클릭하면 얼럿을 나타내는 텍스트를 반환함
+const Hello = () => {
+    // 클릭 시 호출되는 함수
+    const onClick = () => {
+        // 얼럿
+        alert('hello');
+    };
+
+    const text = 'Hello, React';
+
+    // text를 자식으로 갖는 div요소를 반환함
+    // div의 onClick을 클릭 시, 콜백함수를 반환함
+    return <div onClick={onClick}>{text}</div>;
+};
+export default Hello;
